4b1ebedd8eb633305722c0d9ce17e8b9e2a1bff7
JayJayI <barkersmk@gmail.com>
2023-04-25 07:01:23 -0700

    minor changes

--- builtin.c
+++ builtin.c
@@ -14,6 +14,7 @@ int _myexit(info_t *info)
 	if (info->argv[1])  /* If there is an exit arguement */
 	{
 		exitcheck = _erratoi(info->argv[1]);
+
 		if (exitcheck == -1)
 		{
 			info->status = 2;
@@ -26,6 +27,7 @@ int _myexit(info_t *info)
 		return (-2);
 	}
 	info->err_num = -1;
+
 	return (-2);
 }
 
